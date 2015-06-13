6124f2ddff615da11eb7c273966d8c2e2ddd3e4b
Tom LaBruyere <tom.labruyere@gmail.com>
2015-06-13 12:11:42 -0600

    moving code

--- src/main/cpp/LAS1_2.cpp
+++ src/main/cpp/LAS1_2.cpp
@@ -20,13 +20,14 @@ LAS1_2::~LAS1_2() {
 
 void LAS1_2::Read(std::istream& inStream) {
     mPubHeader.Read( inStream );
+
     for (int i=0;i<mPubHeader.NumberOfVariableLengthRecords();i++) {
         LAS1_2VarLenHeader varLenHeader;
         varLenHeader.Read(inStream);
         mVarLenHeaders.push_back(varLenHeader);
         int bytesLeftAferHeader = 0;
-        if ((bytesLeftAferHeader = (varLenHeader.RecordLengthAfterHeader()
-            - LAS1_2VarLenHeader::VAR_HEADER_BYTE_SIZE)) >=0) {
+//        if ((bytesLeftAferHeader = (varLenHeader.RecordLengthAfterHeader()
+//            - LAS1_2VarLenHeader::VAR_HEADER_BYTE_SIZE)) >=0) {
 //            inStream.seekg(bytesLeftAferHeader);
         }
     }
@@ -44,7 +45,7 @@ string LAS1_2::ToString() const {
     ss << "PUBLIC HEADER ===================" << "\n"
         << mPubHeader.ToString() << "\n";
     for (int i=0;i<mVarLenHeaders.size();i++) {
-        ss << "VARIABLE LENGTH HEADER("<< i << "} ==========" << "\n"
+        ss << "VARIABLE LENGTH HEADER("<< i << ") ==========" << "\n"
         << mVarLenHeaders[i].ToString();
     }
     return ss.str();
